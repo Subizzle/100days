ebf5f30b65a9d754bd6aa718c44f5b6313fb95ff
Christopher Snuffer <46074929+Subizzle@users.noreply.github.com>
2022-08-23 18:11:12 -0400

    Update password_length.py
    Improved function by creating "while" loop to ensure correct password requirements were met without ending the program early.

--- password_length.py
+++ password_length.py
@@ -1,7 +1,6 @@
-password = input("Please enter a new password that contains at least 9 characters.") #Asks user to input password.
-if len(password) <=8: #checks to ensure password is proper length.
-    input("Please try again, password must contain at least 9 characters.") #if not correct, informs user to try again.
-elif len(password) >=8: #checks to ensure password is proper length.
-    print("Password successful.") #if correct, tells user it is correct.
+input_string_var = input("Please enter a new password that contains at least 9 characters.") #Asks user to input password.
 
-#need to learn how to add proper loop functionality until password meets criteria. 
+while len(input_string_var) <=8: # Checks to ensure password is proper length.
+    input_string_var = input("Please try again, password must contain at least 9 characters.") # While not correct, informs user to try again.
+
+print("Password successful.") # Once criteria is met, ends program and informs user.
